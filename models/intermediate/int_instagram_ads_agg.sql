f995922aecad458a8691470ec4d115fb0305a935
root <root@ip-10-0-1-30.ec2.internal>
2024-01-10 13:59:10 +0000

    experiment with int model

--- models/intermediate/int_instagram_ads_agg.sql
+++ models/intermediate/int_instagram_ads_agg.sql
@@ -3,4 +3,4 @@ select
     count(distinct user) as total_users,
     sum(time_spent) as total_time_spent,
     avg(time_spent) as avg_time_spent
-from {{ ref('stg_instagram_ads') }}
+from {{ ref('stg_facebook_ads') }}
